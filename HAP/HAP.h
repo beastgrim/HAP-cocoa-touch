4f25540470e6fc6be8859fb3867160f34080cfad
Evgeny Bogomolov <beastgrim@gmail.com>
2018-04-10 12:14:39 +0300

    Initial Commit

---
+++ HAP/HAP.h
@@ -0,0 +1,19 @@
+//
+//  HAP.h
+//  HAP
+//
+//  Created by Евгений Богомолов on 10/04/2018.
+//  Copyright © 2018 Give Me App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HAP.
+FOUNDATION_EXPORT double HAPVersionNumber;
+
+//! Project version string for HAP.
+FOUNDATION_EXPORT const unsigned char HAPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HAP/PublicHeader.h>
+
+
